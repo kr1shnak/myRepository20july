4929057dc862d4166f0a984e82e0e769f1d6d80b
krishna Kanth Dasari <krishnakanth8055@gmail.com>
2022-07-20 01:24:25 +0530

    my java lines of code

--- myfolderone/myjavaregistration.java
+++ myfolderone/myjavaregistration.java
@@ -1,2 +1,9 @@
 this is my registration page
+<<<<<<< Updated upstream
 this my manager work
+=======
+1st day of lines of code
+2nd day of lines of code
+3rd day of lines of code
+4th day of lines of code
+>>>>>>> Stashed changes
